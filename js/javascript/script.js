c42fceeaed8bb24a7ff1e9813b7d1543a178c74e
Denis <kadirovdenis123@gmail.com>
2021-09-02 21:14:52 +0300

    second practic

--- js/javascript/script.js
+++ js/javascript/script.js
@@ -1,13 +1,31 @@
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-let personalMovieDB = {
+let numberOfFilms = prompt("Сколько фильмов посмотрели?", '');
+while(numberOfFilms.length <= 0  || numberOfFilms.length >= 50) {
+    numberOfFilms = prompt("Сколько фильмов посмотрели?", '');
+}
+const personalMovieDB = {
     count: numberOfFilms,
-    movies: new Object(),
-    actors: new Object(),
+    movies: {},
+    actors: {},
     genres: [],
     privat: false
 };
-let ques1 = prompt("Один из последних просмотренных фильмов?", "");
-let ques2 = prompt("'На сколько оцените его?'", "");
-personalMovieDB.movies[ques1] = ques2;
+for(let i = personalMovieDB.count; i > 0; i--) {
+    let a = prompt("Один из последних просмотренных фильмов?", '');
+    while(a.length <= 0  || a.length >= 50) {
+        a = prompt("Один из последних просмотренных фильмов?", '');
+    }
 
-console.log(personalMovieDB);
+    let b = prompt('На сколько оцените его?', '');
+    while(b.length <= 0  || b.length >= 50) {
+        b = prompt('На сколько оцените его?', '');
+    }
+    personalMovieDB.movies[a] = b;
+    
+}
+if(personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count  >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else {
+    alert("Вы киноман");
+}
